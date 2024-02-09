333a6a1885f1c10fb3606bf66be3ab485d699437
black99hard <yasser99hard@gmail.com>
2024-02-10 00:27:24 +0100

    new ip

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["34.229.49.249", "54.144.249.122"]
 
 
 def do_deploy(archive_path):
